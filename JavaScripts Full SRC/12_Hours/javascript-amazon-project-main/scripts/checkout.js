3eae072aa7fe8d229ec715467d12a76ae82f24c9
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-05-04 10:14:17 +0700

    Delete products from cart when clicking delete

--- JavaScripts Full SRC/12_Hours/javascript-amazon-project-main/scripts/checkout.js
+++ JavaScripts Full SRC/12_Hours/javascript-amazon-project-main/scripts/checkout.js
@@ -1,23 +1,19 @@
-import { cart } from "../data/cart.js";
+import { cart,removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
-import {formatCurrency} from "./utils/money.js";
+import { formatCurrency } from "./utils/money.js";
 
-let cartSumaryHTML = '';
-cart.forEach((cartItem)=>{
-    const productId = cartItem.productId;
+let cartSumaryHTML = "";
+cart.forEach((cartItem) => {
+  const productId = cartItem.productId;
 
-    let matchingProduct;
+  let matchingProduct;
 
-    products.forEach((product)=>{
-      if(product.id === productId)
-      {
-         matchingProduct = product;
-      }
-    });
-
-    console.log(matchingProduct);
-
-   cartSumaryHTML += `<div class="cart-item-container">
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+  cartSumaryHTML += `<div class="cart-item-container">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -35,12 +31,15 @@ cart.forEach((cartItem)=>{
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                    Quantity: <span class="quantity-label">${
+                      cartItem.quantity
+                    }</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link 
+                  "data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -91,6 +90,14 @@ cart.forEach((cartItem)=>{
                 </div>
               </div>
             </div>
-          </div>`
+          </div>`;
 });
 document.querySelector(".js-order-summary").innerHTML = cartSumaryHTML;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+     const data = link.dataset.productId;
+     removeFromCart(data);
+     console.log(cart);
+  });
+});
