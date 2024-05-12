f6869420d7a0de9fe73f5efcc3790b5363375bbc
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-05-12 23:28:43 +0700

    Rename

--- JavaScripts Full SRC/12_Hours/javascript-amazon-project-main/tests-jasmine/checkout/orderSummaryTest.js
+++ JavaScripts Full SRC/12_Hours/javascript-amazon-project-main/tests-jasmine/checkout/orderSummaryTest.js
@@ -2,36 +2,39 @@ import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { addToCart, cart, loadFromStogare } from "../../data/cart.js";
 
 describe("test suice: renderOrderSumary", () => {
-  const productId1 = `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`;
-  const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+    
+    const productId1 = `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`;
+    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
 
-  beforeEach(() => {
+  beforeEach(()=>{
     spyOn(localStorage, "setItem");
 
     document.querySelector(
-      ".js-test-container"
-    ).innerHTML = `<div class="js-order-summary"></div>
+        ".js-test-container"
+      ).innerHTML = `<div class="js-order-summary"></div>
                       <div class="js-payment-summary"></div>`;
-    spyOn(localStorage, "setItem");
-    spyOn(localStorage, "getItem").and.callFake(() => {
-      return JSON.stringify([
-        {
-          productId: productId1,
-          quantity: 2,
-          deliveryOptionId: "1",
-        },
-        {
-          productId: productId2,
-          quantity: 1,
-          deliveryOptionId: "1",
-        },
-      ]);
-    });
-    loadFromStogare();
-    renderOrderSummary();
-  });
+      spyOn(localStorage, "setItem");
+      spyOn(localStorage, "getItem").and.callFake(() => {
+        return JSON.stringify([
+          {
+            productId: productId1,
+            quantity: 2,
+            deliveryOptionId: "1",
+          },
+          {
+            productId: productId2,
+            quantity: 1,
+            deliveryOptionId: "1",
+          },
+        ]);
+      });
+      loadFromStogare();
+      renderOrderSummary();
+
+  })
 
   it("display the cart", () => {
+    
     expect(document.querySelectorAll(".js-cart-item-container").length).toEqual(
       2
     );
@@ -44,9 +47,12 @@ describe("test suice: renderOrderSumary", () => {
   });
 
   it("removes a product", () => {
+    
+
+
     document.querySelector(`.js-delete-link${productId1}`).click();
     expect(document.querySelectorAll(".js-cart-item-container").length).toEqual(
-      1
-    );
+        1
+      );
   });
 });
